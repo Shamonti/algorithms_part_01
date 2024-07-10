661d0189441c730d1718f0369cfaf0466f431afa
Shamonti <shamonti.haque98@gmail.com>
2024-07-10 17:58:36 +0600

    Added HelloGoodbye class

--- hello/hello/src/HelloGoodbye.java
+++ hello/hello/src/HelloGoodbye.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 /**
  * HelloGoodbye
  */
 public class HelloGoodbye {
 public static void main(String[] args) {
-  System.out.println("Hello");
+  Scanner myObj = new Scanner(System.in);
+  String names = myObj.nextLine();
+
+  String[] name = names.split(" ");
+
+  System.out.println("Hello " + name[0] + " and " + name[1] + ".");
+  System.out.println("Goodbye " + name[0] + " and " + name[1] + ".");
 }
-  
 }
